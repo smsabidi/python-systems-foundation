6f9a92716cc36ca25934f4ba098aa3ace94196fc
smsabidi <smsabidi@proton.me>
2026-05-22 11:01:53 +0530

    Added the type

--- basic_concepts/recipe_scaller.py
+++ basic_concepts/recipe_scaller.py
@@ -9,7 +9,17 @@ print("Welcome to recipe scaler")
 # Define variables
 pasta_type = "spaghetti"
 quantity = 80
+new_quantity = 12.5
+sale = False
+in_stock = True
 
 # Printing the variables
 print(pasta_type)
 print(quantity)
+
+# Type function
+print(type(pasta_type))
+print(type(quantity))
+print(type(new_quantity))
+print(type(sale))
+print(type(in_stock))
