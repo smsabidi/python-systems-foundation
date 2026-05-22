40881cd3bae61dc47e1ab7178884777f7428e889
smsabidi <smsabidi@proton.me>
2026-05-22 11:03:36 +0530

    Added bool

--- basic_concepts/recipe_scaller.py
+++ basic_concepts/recipe_scaller.py
@@ -23,3 +23,25 @@ print(type(quantity))
 print(type(new_quantity))
 print(type(sale))
 print(type(in_stock))
+
+# Store the number of garlic cloves as an integer
+garlic_cloves = 3
+
+# Store the olive oil amount as a float
+olive_oil_tbsp = 2.5
+print(olive_oil_tbsp)
+
+# Increase the olive oil amount
+new_olive_oil_tbsp = 2.5 + 1
+
+print(garlic_cloves)
+print(new_olive_oil_tbsp)
+
+# Track if you have pasta at home
+has_pasta = True
+
+# Track if you have garlic
+has_garlic = False
+
+print(has_pasta)
+print(has_garlic)
