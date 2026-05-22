e2548610db8515926e09f6aeee329c47187967b1
smsabidi <smsabidi@proton.me>
2026-05-22 10:57:49 +0530

    Added the variables

--- basic_concepts/recipe_scaller.py
+++ basic_concepts/recipe_scaller.py
@@ -5,3 +5,11 @@ Creating recipe scaler
 """
 
 print("Welcome to recipe scaler")
+
+# Define variables
+pasta_type = "spaghetti"
+quantity = 80
+
+# Printing the variables
+print(pasta_type)
+print(quantity)
